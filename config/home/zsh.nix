ca14f5ae5446f02e83cd9702e52a35e719d506d2
Phil Pluckthun <phil@kitten.sh>
2020-06-23 15:03:45 +0100

    Add zsh-syntax-highlighting

--- config/home/zsh.nix
+++ config/home/zsh.nix
@@ -1,12 +1,30 @@
-{
+{ pkgs, ... }:
+
+let
+	sysconfig = (import <nixpkgs/nixos> {}).config;
+in {
   programs.zsh = {
     enable = true;
     enableAutosuggestions = true;
+
+    enableCompletion = !sysconfig.programs.zsh.enable;
 
     shellAliases = {
       ls = "ls --color=auto";
       ll = "ls -l";
     };
+
+    plugins = [
+      {
+        name = "zsh-syntax-highlighting";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-syntax-highlighting";
+          rev = "91d2eeaf23c47341e8dc7ad66dbf85e38c2674de";
+          sha256 = "1160bbhpd2p6qlw1b5k86z243iv0yhv6x7pf414sr8q4cm59x2h0";
+        };
+      }
+    ];
   };
 
   programs.starship = {
