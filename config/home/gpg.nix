997c1e873ca625fabf4b97176d3469fe41ed5315
Phil Pluckthun <phil@kitten.sh>
2021-03-23 23:19:15 +0000

    Add missing sshcontrol file for GPG

--- config/home/gpg.nix
+++ config/home/gpg.nix
@@ -9,4 +9,8 @@ in {
     readSecretFile ../../assets/E2BFF19637FDC25A02F45583176FAD1ED1F6BDD6.key;
   home.file.".gnupg/private-keys-v1.d/CA84692E3CC846C8EC7272468E962B63FC599E49.key".source =
     readSecretFile ../../assets/CA84692E3CC846C8EC7272468E962B63FC599E49.key;
+  home.file.".gnupg/sshcontrol".text = ''
+    E2BFF19637FDC25A02F45583176FAD1ED1F6BDD6
+    75EF1DBB30A59CFB56BCE06A88CCF363DA63B1A7
+  '';
 }
