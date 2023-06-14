a3963833b7186aa74e75fc3533393d0f90d1ff79
Phil Pluckthun <phil@kitten.sh>
2023-06-14 22:49:27 +0100

    Add .gitignore and move secret

--- nix/secrets.nix
+++ nix/secrets.nix
@@ -6,7 +6,7 @@ in rec {
     pkgs.runCommand (baseNameOf (toString path)) {
       buildInputs = [ pkgs.openssl ];
     } ''
-      openssl enc -d -aes-256-cbc -pbkdf2 -salt -base64 -in ${path} -out $out -kfile ${/usr/local/secret}
+      openssl enc -d -aes-256-cbc -pbkdf2 -salt -base64 -in ${path} -out $out -kfile ${../.nix-secret}
     '';
 
   readSecretFileContents = path :
