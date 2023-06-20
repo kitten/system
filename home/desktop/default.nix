fef536a13d11e30f99c5bab9a7f8751231cffaf1
Phil Pluckthun <phil@kitten.sh>
2023-06-20 22:53:46 +0100

    Add mako notifications daemon

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -1,4 +1,4 @@
-{ hyprland, ... }:
+{ pkgs, hyprland, ... }:
 
 {
   imports = [
@@ -9,11 +9,40 @@
 
   wayland.windowManager.hyprland = {
     enable = true;
+    extraConfig = ''
+      bind = SUPER, Return, exec, wezterm
+    '';
   };
 
-  systemd.user.sessionVariables = {
-    "NIXOS_OZONE_WL" = "1"; # force ozone-based electron apps to use Wayland
-    "MOZ_ENABLE_WAYLAND" = "1"; # force Firefox to use Wayland
-    "MOZ_WEBRENDER" = "1";
+  services.mako = {
+    enable = true;
+    font = "sans 10";
+  };
+
+  systemd.user = {
+    services.mako = {
+      Unit = {
+        Description = "Lightweight Wayland notification daemon";
+        PartOf = [ "hyprland-session.target" ];
+        After = [ "hyprland-session.target" ];
+      };
+
+      Install = {
+        WantedBy = [ "hyprland-session.target" ];
+      };
+
+      Service = {
+        Type = "dbus";
+        BusName = "org.freedesktop.Notification";
+        ExecStart = "${pkgs.mako}/bin/mako";
+        ExecReload = "${pkgs.mako}/bin/makoctl reload";
+      };
+    };
+
+    sessionVariables = {
+      "NIXOS_OZONE_WL" = "1"; # force ozone-based electron apps to use Wayland
+      "MOZ_ENABLE_WAYLAND" = "1"; # force Firefox to use Wayland
+      "MOZ_WEBRENDER" = "1";
+    };
   };
 }
