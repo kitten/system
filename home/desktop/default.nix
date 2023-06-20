d7658062aacc127eee9c77ca67e9e27194fa0afe
Phil Pluckthun <phil@kitten.sh>
2023-06-20 21:46:30 +0100

    Add Linux fonts

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -5,7 +5,15 @@
     hyprland.homeManagerModules.default
   ];
 
+  fonts.fontconfig.enable = true;
+
   wayland.windowManager.hyprland = {
     enable = true;
+  };
+
+  systemd.user.sessionVariables = {
+    "NIXOS_OZONE_WL" = "1"; # force ozone-based electron apps to use Wayland
+    "MOZ_ENABLE_WAYLAND" = "1"; # force Firefox to use Wayland
+    "MOZ_WEBRENDER" = "1";
   };
 }
--- modules/fonts/default.nix
+++ modules/fonts/default.nix
@@ -1,40 +1,62 @@
-{ config, pkgs, lib, ... } @ inputs:
+{ config, pkgs, lib, helpers, ... } @ inputs:
 
 let
   inherit (pkgs) stdenv;
 
-  symlink = stdenv.hostPlatform.isLinux;
+  fontsDir = if helpers.isDarwin then
+    "/Library/Fonts" else "/usr/share/fonts/nonfree";
 
-  fontsDir = if stdenv.hostPlatform.isDarwin then
-    "/Library/Fonts" else "/share/fonts/opentype";
-in
+in lib.mkMerge [
+  {
+    age.secrets."DankMono-Regular.otf" = {
+      symlink = false;
+      file = ./encrypt/DankMono-Regular.otf.age;
+      path = "${fontsDir}/DankMono-Regular.otf";
+      mode = "755";
+    };
 
-{
-  age.secrets."DankMono-Regular.otf" = {
-    inherit symlink;
-    file = ./encrypt/DankMono-Regular.otf.age;
-    path = "${fontsDir}/DankMono-Regular.otf";
-    mode = "755";
-  };
+    age.secrets."DankMono-Italic.otf" = {
+      symlink = false;
+      file = ./encrypt/DankMono-Italic.otf.age;
+      path = "${fontsDir}/DankMono-Italic.otf";
+      mode = "755";
+    };
 
-  age.secrets."DankMono-Italic.otf" = {
-    inherit symlink;
-    file = ./encrypt/DankMono-Italic.otf.age;
-    path = "${fontsDir}/DankMono-Italic.otf";
-    mode = "755";
-  };
+    age.secrets."DankMono-Bold.otf" = {
+      symlink = false;
+      file = ./encrypt/DankMono-Bold.otf.age;
+      path = "${fontsDir}/DankMono-Bold.otf";
+      mode = "755";
+    };
 
-  age.secrets."DankMono-Bold.otf" = {
-    inherit symlink;
-    file = ./encrypt/DankMono-Bold.otf.age;
-    path = "${fontsDir}/DankMono-Bold.otf";
-    mode = "755";
-  };
+    age.secrets."codicon.otf" = {
+      symlink = false;
+      file = ./encrypt/codicon.otf.age;
+      path = "${fontsDir}/codicon.otf";
+      mode = "755";
+    };
+  }
 
-  age.secrets."codicon.otf" = {
-    inherit symlink;
-    file = ./encrypt/codicon.otf.age;
-    path = "${fontsDir}/codicon.otf";
-    mode = "755";
-  };
-}
+  (helpers.linuxAttrs {
+    fonts = {
+      enableDefaultFonts = false;
+      fontDir.enable = true;
+
+      fonts = with pkgs; [
+        noto-fonts
+        noto-fonts-cjk
+        noto-fonts-emoji
+        roboto-mono
+        inter
+      ];
+
+      fontconfig.defaultFonts = {
+        serif = [ "Noto Serif" "Noto Color Emoji" ];
+        sansSerif = [ "Inter" "Noto Color Emoji" ];
+        monospace = [ "Dank Mono" "Roboto Mono" "Noto Color Emoji" ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+
+    };
+  })
+]
