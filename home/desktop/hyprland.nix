d821a269a1845aa34e6ffbadfa05a536380e372b
Phil Pluckthun <phil@kitten.sh>
2023-06-21 02:26:15 +0100

    Sync up home/modules for hyprland xwayland options

--- home/desktop/hyprland.nix
+++ home/desktop/hyprland.nix
@@ -7,10 +7,18 @@
 
   wayland.windowManager.hyprland = {
     enable = true;
+    xwayland = {
+      enable = true;
+      hidpi = true;
+    };
     extraConfig = ''
       bind = SUPER, Return, exec, wezterm
       bind = SUPER, Space, exec, wofi
       bind = SUPER, Q, killactive
+
+      exec-once=xprop -root -f _XWAYLAND_GLOBAL_OUTPUT_SCALE 32c -set _XWAYLAND_GLOBAL_OUTPUT_SCALE 1.44
+      env = GDK_SCALE, 2
+      env = XCURSOR_SIZE, 32
     '';
   };
 }
