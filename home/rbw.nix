cdebd2629a34b465ebb52267e22b1dfcf2004b82
Phil Pluckthun <phil@kitten.sh>
2023-06-22 12:50:09 +0100

    Fix rbw config for darwin

--- home/rbw.nix
+++ home/rbw.nix
@@ -1,14 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, helpers, ... }:
 
 {
-  home.packages = [ pkgs.rofi-rbw ];
-
   programs.rbw = {
     enable = true;
     settings = {
       email = "phil@kitten.sh";
       base_url = "https://vault.kitten.sh";
-      pinentry = "gnome3";
+      pinentry = if helpers.isDarwin then "curses" else "gnome3";
     };
   };
 
