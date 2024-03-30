825b309c90fab1cbbbd4ee04c75703557debeedb
Phil Pluckthun <phil@kitten.sh>
2024-03-30 22:44:01 +0000

    Add comments to colours

--- lib/colors.nix
+++ lib/colors.nix
@@ -14,28 +14,48 @@ in rec {
   darkStroke = "rgba(22, 23, 29, 0.2)";
 
   hex = {
+    # for sidebars / sidenavs / passive UI elements
     gutter = "161622";
+    # for selection / cursors
     cursor = "242738";
+    # for overlays / large popups / hint / codelens UIs
     element = "0F0E17";
+    # for borders and thin lines, e.g. to separate panes
     split = "2A2D46";
 
+    # (ANSI: 0 Black) background colour
     black = "0F1018";
-    grey = "323744";
+    # (ANSI: 7 White) foreground colour, identifiers, variables
     white = "EDF0F2";
+    # (ANSI: 1 Bright Black) ghost text / suggestion text
+    grey = "323744";
+    # (ANSI: 15 Bright White) comment text
     muted = "585D79";
 
+    # (ANSI: 1 Red) conditional, boolean, return keywords
     red = "F84F6B";
+    # (ANSI: 9 Bright Red) errors
     brightRed = "E93434";
+    # (ANSI: 2 Green) strings
     green = "7DD486";
+    # (ANSI: 10 Bright Green) special chars
     brightGreen = "05AE48";
+    # (ANSI: 3 Yellow) warnings & structures
     yellow = "D7C046";
+    # (ANSI: 11 Bright Yellow) constants & numbers
     orange = "F95A10";
+    # (ANSI: 4 Blue) functions, search, and some selections
     blue = "98BDFB";
+    # (ANSI: 12 Bright Blue) todos & URL/links
     brightBlue = "6A78F6";
-    pink = "F2B0C0";
+    # (ANSI: 5 Purple) keywords, statements
     magenta = "F651A6";
-    cyan = "02B394";
+    # (ANSI: 13 Bright Purple) operators, tags, properties
+    pink = "F2B0C0";
+    # (ANSI: 6 Cyan) types
     aqua = "15C0CB";
+    # (ANSI: 14 Bright Cyan) delimiters & labels
+    cyan = "02B394";
   };
 
   colors = {
