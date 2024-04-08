64de35595583eb4c9cc9727f1ff989208152d588
Phil Pluckthun <phil@kitten.sh>
2024-04-08 17:31:48 +0100

    Adjust privileges of HASS container

--- modules/server/home-assistant.nix
+++ modules/server/home-assistant.nix
@@ -1,6 +1,14 @@
-{ ... }:
+{ config, ... }:
 
 {
+  users = {
+    groups.hass.gid = config.ids.gids.hass;
+    users.hass = {
+      uid = config.ids.uids.hass;
+      group = "hass";
+    };
+  };
+
   virtualisation.oci-containers = {
     backend = "podman";
     containers.hass = {
@@ -8,10 +16,14 @@
       volumes = [
         "/var/lib/home-assistant:/config"
         "/etc/localtime:/etc/localtime:ro"
+        "/sys:/sys:ro"
       ];
       environment.TZ = "Europe/London";
       image = "ghcr.io/home-assistant/home-assistant:stable";
       extraOptions = [
+        "--userns=keep-id"
+        "--hostuser=hass"
+        "--runtime=runc"
         "--device=/dev/ttyUSB0"
         "--network=host"
         "--privileged"
