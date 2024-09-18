b2b24820bbb3866e03b6e39ce60d4df30a9e32da
Phil Pluckthun <phil@kitten.sh>
2024-09-18 17:08:03 +0100

    Update vaultwarden DOMAIN

--- modules/server/vaultwarden.nix
+++ modules/server/vaultwarden.nix
@@ -2,8 +2,8 @@
 
 with lib;
 let
-  cfgRoot = config.modules.server;
-  cfg = config.modules.server.vaultwarden;
+  address = config.modules.router.adress;
+  cfg = config.modules.server;
 in {
   options.modules.server.vaultwarden = {
     enable = mkOption {
@@ -14,8 +14,13 @@ in {
     };
   };
 
-  config = mkIf (cfg.enable && cfgRoot.enable) {
-    services.vaultwarden = {
+  config = mkIf (cfg.enable && cfg.vaultwarden.enable) {
+    services.vaultwarden = let
+      baseURL = if (cfg.caddy.enable && cfg.tailscale.enable)
+        then "https://${hostname}.fable-pancake.ts.net/vault/"
+        else if cfg.caddy.enable then "http://${address}/vault/"
+        else "http://${address}:8000/vault/";
+    in {
       enable = true;
       dbBackend = "sqlite";
       config = {
@@ -23,7 +28,7 @@ in {
         ADMIN_TOKEN = "$argon2id$v=19$m=65540,t=3,p=4$+5A5H6YiN6OxyrFggkrft8Mm+sxgh/tL3USbaYFZ/h8$qj8NjE+COL4WXjmjkPWSQk7iLfhaBfBtV6k06Bql3CQ";
         PASSWORD_HINTS_ALLOWED = "false";
         SIGNUPS_ALLOWED = "false";
-        DOMAIN = "https://${hostname}.fable-pancake.ts.net";
+        DOMAIN = baseURL;
         WEBSOCKET_ADDRESS = "127.0.0.1";
         ROCKET_ADDRESS = "127.0.0.1";
         WEBSOCKET_PORT = "8001";
