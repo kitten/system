a016dba7002466287d4bd6afd3af234b2bf7bd59
Phil Pluckthun <phil@kitten.sh>
2024-12-03 15:15:43 +0000

    Update flakes

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-1.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-2.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
