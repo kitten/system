1604a2776473bcee6bcd429f772f17007fb722c5
Phil Pluckthun <phil@kitten.sh>
2025-02-21 21:09:01 +0000

    Update palworld to 2025-02-21

--- lib/pkgs/palworld-server.nix
+++ lib/pkgs/palworld-server.nix
@@ -6,11 +6,11 @@ self: {
 with lib;
 self.mkSteamPackage {
   name = "palworld-server";
-  version = "17082920";
+  version = "17403768";
   appId = "2394010";
   depotId = "2394012";
-  manifestId = "2423583208459052375";
-  hash = "sha256-gAFEDf/rKPQ5zTH8EJ93e4KKHUGi8uiYlPS7G2lWGWk=";
+  manifestId = "1367771460964183113";
+  hash = "sha256-L/lRfVmc3tkMiPSPJSg6uum0/DJB97dkuPzOA3LtDEw=";
   meta = {
     description = "Palworld Dedicated Server";
     homepage = "https://steamdb.info/app/2394010/";
