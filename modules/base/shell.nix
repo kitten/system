8a0ce26d145c88f9ca9dbc9841f6ef3546e53703
Phil Pluckthun <phil@kitten.sh>
2025-02-27 20:10:17 +0000

    Add missing clipboard utils

--- modules/base/shell.nix
+++ modules/base/shell.nix
@@ -1,15 +1,21 @@
-{ lib, pkgs, ... }:
+{ lib, helpers, pkgs, ... }:
 
-{
-  environment.systemPackages = [ pkgs.zsh ];
+with lib;
+mkMerge [
+  {
+    environment.systemPackages = [ pkgs.zsh ];
 
-  environment.pathsToLink = [ "/share/zsh" ];
-  environment.variables = {
-    SHELL = "${pkgs.zsh}/bin/zsh";
-  };
+    environment.pathsToLink = [ "/share/zsh" ];
+    environment.variables = {
+      SHELL = "${pkgs.zsh}/bin/zsh";
+    };
 
-  programs.zsh = {
-    enable = true;
-    promptInit = lib.mkDefault "";
-  };
-}
+    programs.zsh = {
+      enable = true;
+      promptInit = lib.mkDefault "";
+    };
+  }
+  (helpers.linuxAttrs {
+    environment.systemPackages = [ pkgs.wl-clipboard-rs ];
+  })
+]
