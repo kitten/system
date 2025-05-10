6046a26f9d7d50fd4cf03e548edfdf73898b7b5c
Phil Pluckthun <phil@kitten.sh>
2025-05-10 21:42:49 +0100

    Add desktop tools

---
+++ home/desktop/tools.nix
@@ -0,0 +1,21 @@
+{ lib, config, pkgs, ... }:
+
+with lib;
+let
+  cfg = config.modules.desktop;
+in {
+  options.modules.desktop.tools = {
+    enable = mkOption {
+      default = cfg.enable;
+      example = true;
+      description = "Whether to enable basic desktop tools.";
+      type = types.bool;
+    };
+  };
+
+  config = mkIf cfg.tools.enable {
+    home.packages = with pkgs; [
+      mission-center
+    ];
+  };
+}
