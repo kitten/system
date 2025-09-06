5ab903bffe615d15d1892ca2c87520132bb2187b
Phil Pluckthun <phil@kitten.sh>
2025-09-06 15:11:47 +0100

    Update ssh config

--- home/base/ssh.nix
+++ home/base/ssh.nix
@@ -4,10 +4,19 @@ with lib;
 {
   programs.ssh = {
     enable = true;
-    compression = true;
-    serverAliveCountMax = 5;
-    serverAliveInterval = 60;
+    enableDefaultConfig = false;
     matchBlocks."*" = {
+      forwardAgent = false;
+      addKeysToAgent = "no";
+      hashKnownHosts = false;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+      controlMaster = "no";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "no";
+
+      compression = true;
+      serverAliveInterval = 60;
+      serverAliveCountMax = 5;
       extraOptions.IPQoS = "none";
       setEnv.TERM = "xterm";
     };
