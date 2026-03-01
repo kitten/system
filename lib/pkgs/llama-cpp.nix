729a07c7cb1dda2d4384315c61f007a171fe571a
Phil Pluckthun <phil@kitten.sh>
2026-03-01 16:45:44 +0000

    Update llama-cpp override

--- lib/pkgs/llama-cpp.nix
+++ lib/pkgs/llama-cpp.nix
@@ -6,12 +6,19 @@ self: pkgs @ {
 }:
 
 with lib;
-llama-cpp.overrideAttrs (old: rec {
+llama-cpp.overrideAttrs (finalAttrs: prevAttrs: rec {
   version = "8183";
   src = fetchFromGitHub {
     owner = "ggml-org";
     repo = "llama.cpp";
-    rev = "b${version}";
-    hash = "sha256-Cmma3SwcGiJJ87nCXUMJvB56La8+BSAu4YiuOxxQrWA=";
+    tag = "b${version}";
+    hash = "sha256-ElVxZ9iWg7fCGWy+ttsLlCESasLt5GrRAKpUjoKaOdg=";
+    leaveDotGit = true;
+    postFetch = ''
+      git -C "$out" rev-parse --short HEAD > $out/COMMIT
+      find "$out" -name .git -print0 | xargs -0 rm -rf
+    '';
   };
+
+  npmDepsHash = "sha256-FKjoZTKm0ddoVdpxzYrRUmTiuafEfbKc4UD2fz2fb8A=";
 })
