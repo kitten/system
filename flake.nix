202a9024699c27d2656a48aeab36985003eed207
Phil Pluckthun <phil@kitten.sh>
2026-06-05 13:27:01 +0100

    Update flake locks

--- flake.nix
+++ flake.nix
@@ -5,7 +5,11 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
+    nixos-hardware = {
+      url = "github:NixOS/nixos-hardware/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     apple-silicon = {
       url = "github:nix-community/nixos-apple-silicon";
