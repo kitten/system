53c467e3de1251b043ab192e94821b0c074975a6
Phil Pluckthun <phil@kitten.sh>
2026-08-23 15:49:34 +0100

    Drop vesktop

--- home/apps/discord.nix
+++ home/apps/discord.nix
@@ -14,28 +14,7 @@ in {
 
   config = mkIf (cfg.enable && cfg.discord.enable) (mkMerge [
     {
-      home.packages = with pkgs; let
-        pkg = vesktop.overrideAttrs (old: {
-          # electron builds must be writable
-          preBuild = optionalString stdenv.hostPlatform.isDarwin ''
-            cp -r ${electron.dist}/Electron.app .
-            chmod -R u+w Electron.app
-          '' + optionalString stdenv.hostPlatform.isLinux ''
-            cp -r ${electron.dist} electron-dist
-            chmod -R u+w electron-dist
-          '';
-          buildPhase = ''
-            runHook preBuild
-            pnpm build
-            pnpm exec electron-builder \
-              --dir \
-              -c.asarUnpack="**/*.node" \
-              -c.electronDist=${if stdenv.hostPlatform.isDarwin then "." else "electron-dist"} \
-              -c.electronVersion=${electron.version}
-            runHook postBuild
-          '';
-        });
-      in [ pkg ];
+      home.packages = [ pkgs.discord ];
     }
 
     (helpers.mkIfLinux {
